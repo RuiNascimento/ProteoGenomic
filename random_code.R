149bd81da2af52448e49b5671bb90e9ff92e7d99
Rui Nascimento <rui_nascimento93@hotmail.com>
2019-12-13 10:41:42 +0000

    minor update

--- random_code.R
+++ random_code.R
@@ -1,5 +1,6 @@
 library(httr)
 UniprotConvertion <- function(query, from, to, format = 'tab') {
+  ## Convert identifiers with the help of UNIPROT
   if (length(query)>1) {query <- paste0(query, collapse = " ")}
   b2 <- "https://www.uniprot.org/uploadlists/"
   response <- POST(b2, body = list('from' = from, 'to' = to, 'format' = format,
@@ -13,6 +14,7 @@ UniprotConvertion(c("296083299", "225431846", "147773334", "225431854", "1002416
 getUniprotGoodies <- function(query, columns = c("id","entry name","reviewed","protein names","genes","organism","length"))
 {
   ## Set columns with some defaults
+  ## https://www.uniprot.org/help/uniprotkb_column_names to check the possible column names
   ## query and columns start as a character vectors
   qstring <- paste(query, collapse="+or+")
   cstring <- paste(columns, collapse=",")
@@ -39,4 +41,20 @@ getGI <- function(s) {
 s <- "gi|359491770|ref|XP_003634320.1|;gi|225439342|ref|XP_002270170.1|;gi|225437695|ref|XP_002279878.1|;gi|147853527|emb|CAN80663.1|;gi|147836392|emb|CAN75420.1|;gi|147834215|emb|CAN70884.1|;gi|147767872|emb|CAN71283.1|;gi|302143958|emb|CBI23063.3|;gi|297740140|emb|CBI30322.3|;gi|225455266|ref|XP_002273568.1|;gi|147769165|emb|CAN60770.1|;gi|147802377|emb|CAN77120.1|;gi|297736166|emb|CBI24204.3|;gi|225465030|ref|XP_002265864.1|;gi|297735960|emb|CBI23934.3|;gi|302143596|emb|CBI22349.3|;gi|731432763|ref|XP_010644407.1|;gi|225465609|ref|XP_002266370.1|;gi|731432761|ref|XP_010644406.1|;gi|731432759|ref|XP_010644405.1|;gi|731432757|ref|XP_010644404.1|;gi|147805226|emb|CAN64480.1|;gi|225465615|ref|XP_002267017.1|;gi|147834511|emb|CAN71997.1|;gi|302143601|emb|CBI22354.3|;gi|731417327|ref|XP_002267206.2|;gi|297744041|emb|CBI37011.3|;gi|76559884|tpe|CAI56329.1|;gi|297742776|emb|CBI35456.3|"
 getGI(s)
 
+query <- getGI(s)
+converted <- UniprotConvertion(query = query, from = "P_GI", "ACC")
+head(converted)
+getUniprotGoodies(converted$To)
+
+spl <- str_split(s, ";")
+spl
+q <- str_split_fixed(spl[[1]], "\\|",n = 5)
+
+library(data.table)
+
+#q <- str_split_fixed(t[[1]], "\\|",n = 3)
+head(q)
+df <- data.frame("gi" = q[,2], "ref" = q[,3], "id" = q[,4])
+
+head(df)
 
